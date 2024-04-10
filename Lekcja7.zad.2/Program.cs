cbb5e7040bfc9bc3e94638a42540eb416d175efa
Wojciech Zawadzki <zawadzki1993@hotmail.com>
2024-04-10 19:38:06 +0200

    Add project files.

---
+++ Lekcja7.zad.2/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.Write("Podaj liczbę całkowitą: ");
+		int liczba = Convert.ToInt32(Console.ReadLine());
+
+		if (liczba % 2 == 0)
+		{
+			Console.WriteLine($"{liczba} jest liczbą parzystą.");
+		}
+		else
+		{
+			Console.WriteLine($"{liczba} jest liczbą nieparzystą.");
+		}
+
+		Console.ReadLine();
+	}
+}
